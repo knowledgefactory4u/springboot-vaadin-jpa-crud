ae3ad21bcff594691e09473b5896c062e7770608
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2022-06-23 19:18:54 +0530

    Add files via upload
    initi

---
+++ src/main/java/com/knf/dev/demo/springvaadincrud/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.knf.dev.demo.springvaadincrud.backend.repository;
+
+import com.knf.dev.demo.springvaadincrud.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository
+                extends JpaRepository<User, Long> {
+
+    List<User> findByEmailStartsWithIgnoreCase(String email);
+}
